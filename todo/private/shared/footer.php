804e0e536536f92c1fa0804e01934af1f030d06f
Christian Knauer <christiancalled@gmail.com>
2019-01-22 08:58:12 +0100

    add shared header and footer

---
+++ todo/private/shared/footer.php
@@ -0,0 +1,6 @@
+	<footer>
+		<!--dynamic year-->
+		&copy; <?php echo date('Y'); ?> To-do List
+	</footer>
+</body>
+</html>
---
+++ todo/private/shared/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>TODO</title>
+	<link rel="stylesheet" media="all" href="stylesheets/style.css">
+</head>
+<body>
+	<header>
+		<h2>to-do list</h2>
+	</header>
