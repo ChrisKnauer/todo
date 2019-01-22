d536af7e61fba73be488aa8dda65c09f64c82cce
Christian Knauer <christiancalled@gmail.com>
2019-01-22 08:23:05 +0100

    create and style first page

--- todo/public/index.php
+++ todo/public/index.php
@@ -1,1 +1,14 @@
 <?php  // root of our app ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>TODO</title>
+</head>
+<body>
+	<header>
+		<h1>login: coming soon</h1>
+	</header>
+</body>
+</html>
