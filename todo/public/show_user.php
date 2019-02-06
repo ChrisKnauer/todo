3fb003021ec625256b4120ed33aab0d0550826df
Christian Knauer <christiancalled@gmail.com>
2019-02-06 10:01:33 +0100

    Read user
    1) in show_user.php get user id from session. then call find_user_by_id.
    2) display username
    3) link to delete_user.php, send user id in query string
    4) create delete_user.php

--- todo/public/show_user.php
+++ todo/public/show_user.php
@@ -7,16 +7,20 @@ $h2 = 'User details';
 include(SHARED_PATH . '/header.php');
 include(SHARED_PATH . '/header_login.php');
 
+
+$id = $_SESSION['user_id'];
+$user = find_user_by_id($id);  // assoc. array
+
 ?>
 
 <div id="content">
 
 	<a href="<?php echo WWW_ROOT . '/list.php' ?>">&laquo;Back to list</a> <br><br>
 
-	Username: xxxxx <br><br>
+	Username: <?php echo h($user['username']); ?> <br><br>
 
 	<a href="#">Update password</a> <br><br>
-	<a href="#">Delete user</a> <br>
+	<a href="<? echo WWW_ROOT . "/delete_user.php?id=" . h(urlencode($user['id'])); ?>">Delete user</a> <br>
 
 </div>
 
